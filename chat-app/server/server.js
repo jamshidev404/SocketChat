4e020c8175dfb39ed33f6c82519ae6bbd485265c
jamshidev404 <jamshidbek536@gmail.com>
2022-06-16 23:14:24 +0500

    ddd

--- chat-app/server/server.js
+++ chat-app/server/server.js
@@ -1,13 +1,27 @@
 const express = require("express")
 const path = require('path')
+const http = require('http'); 
+const socketIO = require('socket.io');
+const { disconnect } = require("process");
 
 const app = express();
 const publicPath = path.join(__dirname, '/../public')
 
+let server = http.createServer(app)
+let io = socketIO(server)
+
 app.use(express.json())
 app.use(express.static(publicPath))
 
+io.on("connection", (socket) => {
+    console.log("A new user just connected");
+
+    socket.on("disconnect", () => {
+        console.log("User was disconnect");
+    })
+})
+
 const port = process.env.PORT || 3300
-app.listen(port, () => {
+server.listen(port, () => {
     console.log("Server running");
 }) 
